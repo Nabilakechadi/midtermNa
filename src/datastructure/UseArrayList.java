e74a1db2114ba3f85892e2c0213c7db3bb0ba819
nabila kechadi <nkechadi02@gmail.com>
2022-12-24 16:55:26 -0700

    data structure

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,8 +1,14 @@
 package datastructure;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 import  java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-public class UseArrayList {
+public  class UseArrayList {
 
 	public static void main(String[] args) {
 		/*
@@ -11,34 +17,10 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 *
 		 */
-		/*public static void main(String[] args)
-		{
-			ArrayList<String> list1= new ArrayList<String>();
-			System.out.println("Size of ArrayList: "+list1.size());
-			//Adding the elements
-			list1.add("Java");
-			list1.add("JDBC");
-			System.out.println("Elements of first ArrayList: "+al);
-
-			ArrayList<String> al2 = new ArrayList<String>();
-			al2.add("EJB");
-			al2.add("Struts");
-			//Adding the both array
-			al2.addAll(al
-			System.out.println("Elements of second ArrayList: "+al2);
-			//remove the element
-			al2.remove("EJB");
-			System.out.println("Elements of ArrayList after deletion: "+al2);
-			System.out.println("Size of ArrayList: "+al2.size());
-
-			//Retriving 2nd index element
-			System.out.println("The element at 2nd index is: "+al2.get(2));
-		}
-	}
 
 		List<String> list = new ArrayList<String>();
 
-//Insert ten elements into list
+
 		list.add("One");
 		list.add("Two");
 		list.add("Thee");
@@ -52,7 +34,7 @@ public class UseArrayList {
 
 		System.out.println("FOR EACH LOOP");
 
-		for(String s : list) {
+		for (String s : list) {
 			System.out.println(s);
 		}
 
@@ -60,42 +42,27 @@ public class UseArrayList {
 
 		Iterator<String> it = list.listIterator();
 
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			System.out.println(it.next());
 		}
 		it = list.listIterator();
-		list.remove(7);
 		System.out.println("REMOVE ELEMENT");
-		while(it.hasNext()) {
-			if(it.next().contentEquals("Eight"))
+		while (it.hasNext()) {
+			if (it.next().contentEquals("Eight"))
 				it.remove();
 		}
 		it = list.listIterator();
 		System.out.println("AFTER REMOVING EIGHT");
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			System.out.println(it.next());
 		}
-	}
 
-}*/
-
-		ArrayList<String> list = new ArrayList<String>();
-		list.add("one");
-		list.add("two");
-		list.add("three");
-		list.add("four");
-		list.add("five");
-		list.add("six");
-		list.add("seven");
-		list.add("eight");
-		list.add("nine");
-		list.add("ten");
-
-			System.out.println(list);
-			list.remove(6);
-		System.out.println(list);
 		}
+
 	}
+
+
+
 
 
 
--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,14 +1,47 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.LinkedList;
 public class UseQueue {
 
 	public static void main(String[] args) {
-		/*
-		 * Demonstrate how to use Queue that includes add,peek,remove,pool elements.
-		 * Use For Each loop and while loop with Iterator to retrieve data.
-		 * 
-		 */
 
-	}
+				Queue<Integer> q = new LinkedList<>();
 
-}
+
+				int i = 0;
+				while (i < 5) {
+					q.add(i);
+					i++;
+				}
+
+
+				System.out.println("Elements of queue : ");
+				for (int num : q)
+					System.out.println(num + " ");
+
+				System.out.println("");
+
+
+				int head = q.peek();
+				System.out.println("head of queue-" + head);
+
+
+				int first = q.remove();
+				System.out.println("removed element-" + first);
+
+				System.out.println("Elements of queue : ");
+				Iterator it = q.iterator();
+				while (it.hasNext()) {
+					System.out.println(it.next());
+				}
+				System.out.println("");
+
+				int pollelement = q.poll();
+				System.out.println("Queue's head: " + pollelement);
+
+			}
+		}
+
+
