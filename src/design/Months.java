604e9614bfceaa660e12caa7e597ffc019762238
nabila kechadi <nkechadi02@gmail.com>
2022-12-30 11:55:02 -0700

    second push

--- src/design/Months.java
+++ src/design/Months.java
@@ -2,12 +2,6 @@ package design;
 import java.time.*;
 public enum Months {
     January, February, March, April, May, June, July, August, September, October, November, December;
-    public static void main(String[] args) {
-            Month month = Month.from(LocalDate.now());
-
-            System.out.println(month.name());
-        }
-    }
-
+}
 
 
